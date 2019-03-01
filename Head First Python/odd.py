81b81c13615a82046bc615e4f4d9ff561370132f
mtjikuzu <mtjikuzu@gmail.com>
2019-03-01 18:19:46 +0200

    experiment with range to fill list

--- Head First Python/odd.py
+++ Head First Python/odd.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import time
 import random
 
+# Fill the odd numbers list with range command instead of the manual way below. 
+odds_experiment = list(range(1,61,2)) 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27,
         29, 31, 33 ,35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
